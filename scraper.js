65f81573c1972d15cb1e757f2c56a2e9d8dc3347
Jorg <jvdham@gmail.com>
2015-02-12 15:49:29 +0000

    added loop functionality

--- scraper.js
+++ scraper.js
@@ -1,42 +1,53 @@
 var Spooky = require('spooky');
 
 exports.scrapeKickstarterUrls = function() {
-  var spooky = new Spooky({
-      child: {
-       'ignore-ssl-errors': 'yes',
-       'ssl-protocol': 'any'
-      },
-      casper: {
-        logLevel: 'debug',
-        verbose: true
-      }
+    var spooky = new Spooky({
+        child: {
+            // kickstarter uses https so we need to set phantomJS to utilise ssl
+            'ignore-ssl-errors': 'yes',
+            'ssl-protocol': 'any'
+        },
+        casper: {
+            logLevel: 'debug',
+            verbose: true
+        }
     }, function(err) {
-
-       spooky.start("https://www.kickstarter.com/discover/advanced?sort=magic");
-       spooky.then(function() {
-        var links = this.evaluate(function () {
-        var projectPreview = document.querySelectorAll('#projects_list > li > div > div.project-thumbnail > a');
-        return Array.prototype.map.call(projectPreview, function(e) {
-          return e.getAttribute('href');
-        });
-      });
-         this.emit("doSomethingWithLinks", links)
-       });
-      spooky.run();
+        function getLinks() {
+            var links = this.evaluate(function() {
+                // get all the links on a page
+                var projectPreview = document.querySelectorAll('#projects_list > li > div > div.project-thumbnail > a');
+                return Array.prototype.map.call(projectPreview, function(e) {
+                    return e.getAttribute('href');
+                });
+            });
+            // this sends the code to the listener
+            this.emit("doSomethingWithLinks", links)
+                // clicks on the 'show more button'
+            this.click('#projects > div.container-flex.px2 > div > a');
+            // limit the amount of links you want to scrape
+            if (links.length < 200) {
+                this.wait(2000, function() {
+                    getLinks.call(this);
+                })
+            }
+        }
+        spooky.start("https://www.kickstarter.com/discover/advanced?sort=magic");
+        spooky.then(getLinks);
+        spooky.run();
     })
-  spooky.on('doSomethingWithLinks', function(links) {
-    // here you can add logic
-    console.log(links);
-  });
-  // logging
-  spooky.on('console', function(line) {
-    console.log(line);
-  });
-  spooky.on('error', function (e, stack) {
-   console.error(e);
+    spooky.on('doSomethingWithLinks', function(links) {
+        // here you can add logic to save the links to a database
+        console.log(links);
+    });
+    // logging
+    spooky.on('console', function(line) {
+        console.log(line);
+    });
+    spooky.on('error', function(e, stack) {
+        console.error(e);
 
-    if (stack) {
-        console.log(stack);
-    }
-  });
-  };
+        if (stack) {
+            console.log(stack);
+        }
+    });
+};
--- server.js
+++ server.js
@@ -2,14 +2,6 @@ var express = require('express'),
     app = express(),
     scrape = require('./scraper.js');
 
+// start scraping the Kickstarter urls
 scrape.scrapeKickstarterUrls();
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-})
 
